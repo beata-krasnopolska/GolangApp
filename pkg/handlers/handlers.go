ffe4c869cc94b54bd9043a50e193846234b82fc1
Beata Krasnopolska <bkrasnopolska@objectivity.co.uk>
2023-09-28 19:19:45 +0200

    add simple chache

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"text/template"
 )
@@ -16,12 +17,10 @@ func About(w http.ResponseWriter, r *http.Request) {
 	RenderTemplate(w, "about.page.html")
 }
 
-// small letter in function name is like making it private
-func addValues(x, y int) int {
-	return x + y
-}
+// package lever variable for template cache
+var tc = make(map[string]*template.Template)
 
-func RenderTemplate(w http.ResponseWriter, tmpl string) {
+func renderTemplateTest(w http.ResponseWriter, tmpl string) {
 	parsedTemplate, _ := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.html")
 	err := parsedTemplate.Execute(w, nil)
 
@@ -29,4 +28,48 @@ func RenderTemplate(w http.ResponseWriter, tmpl string) {
 		fmt.Println("error parsing template: ", err)
 		return
 	}
+}
+
+func RenderTemplate(w http.ResponseWriter, t string) {
+	var tmpl *template.Template
+	var err error
+
+	// check to see if we alredy have the template in our chache
+	_, inMap := tc[t]
+	if !inMap {
+		// need to create the template
+		log.Println("Creating template and adding to cache")
+		err = createTemplateCache(t)
+		if err != nil {
+			log.Println(err)
+		}
+	} else {
+		// we have the templete in the cache
+		log.Println("using cached template")
+	}
+
+	tmpl = tc[t]
+
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func createTemplateCache(t string) error {
+	templates := []string{
+		fmt.Sprintf("./templates/%s", t),
+		"./templates/base.layout.html",
+	}
+
+	// parse the template
+	tmpl, err := template.ParseFiles(templates...)
+	if err != nil {
+		return err
+	}
+
+	// add template to cache
+	tc[t] = tmpl
+
+	return nil
 }
