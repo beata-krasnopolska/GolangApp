6e2cce7cf97c485952a2a115dbe0bdd173306d52
Beata Krasnopolska <bkrasnopolska@objectivity.co.uk>
2023-09-29 12:22:16 +0200

    assign template cache

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -15,15 +15,23 @@ func main() {
 
 	var app config.AppConfig
 
+	//get template cache
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
 		log.Fatal("cannot create template cache")
 	}
 
+	//assign template cache
 	app.TemplateCache = tc
+	app.UseCache = false
 
-	http.HandleFunc("/", handlers.Home)
-	http.HandleFunc("/about", handlers.About)
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	http.HandleFunc("/", handlers.Repo.Home)
+	http.HandleFunc("/about", handlers.Repo.About)
 
 	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
 	_ = http.ListenAndServe(portNumber, nil)
