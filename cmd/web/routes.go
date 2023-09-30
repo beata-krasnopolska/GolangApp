15a96da3e8b74fdb1ab5ec35d6b57d903956523e
Beata Krasnopolska <bkrasnopolska@objectivity.co.uk>
2023-09-30 23:12:11 +0200

    Add Recoverer middleware in routes.go from chi package

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -6,11 +6,13 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func routes(app *config.AppConfig) http.Handler {
 
 	mux := chi.NewRouter() //create multiplex
+	mux.Use(middleware.Recoverer)
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.21.1
 
 require github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f
 
-require github.com/go-chi/chi v1.5.5
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/go-chi/chi/v5 v5.0.10
+)
