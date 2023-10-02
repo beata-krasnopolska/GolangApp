8a1febc69af2a3e4392ce9d1f749b2fef732f0a5
Beata Krasnopolska <bkrasnopolska@objectivity.co.uk>
2023-10-02 18:50:02 +0200

    sessions check

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -29,6 +29,10 @@ func NewHandlers(r *Repository) {
 
 // Home is the home page handler
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	// grab remote user IP address
+	remoteIp := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIp)
+
 	render.RenderTemplate(w, "home.page.html", &models.TemplateData{})
 }
 
@@ -37,6 +41,9 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	// preform some logic
 	stringMap := make(map[string]string)
 	stringMap["test"] = "Hello, again on ABOUT PAGE"
+
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+	stringMap["remote_ip"] = remoteIP
 
 	render.RenderTemplate(w, "about.page.html", &models.TemplateData{
 		StringMap: stringMap,
